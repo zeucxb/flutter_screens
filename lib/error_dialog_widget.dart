fb36ced22eda34fb85d64c11acc666017bf301a9
Eliseu Codinhoto <zeucxb@gmail.com>
2020-01-04 16:25:27 -0300

    first release

---
+++ lib/error_dialog_widget.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
+
+class ErrorDialog extends StatelessWidget {
+  final String errorMessage;
+  final bool previousStatusBarWhiteForeground;
+  final Function closeFunction;
+
+  ErrorDialog({
+    Key key,
+    @required this.errorMessage,
+    this.previousStatusBarWhiteForeground,
+    @required this.closeFunction,
+  })  : assert(errorMessage != null && errorMessage.isNotEmpty),
+        assert(closeFunction != null),
+        super(key: key);
+
+  _closeDialog() {
+    if (previousStatusBarWhiteForeground != null) {
+      FlutterStatusbarcolor.setStatusBarWhiteForeground(false);
+    }
+
+    closeFunction();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (errorMessage == null) return Container();
+
+    FlutterStatusbarcolor.setStatusBarWhiteForeground(true);
+
+    return Container(
+      color: Colors.red,
+      child: SafeArea(
+        child: Material(
+          child: Container(
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            color: Colors.red,
+            width: double.infinity,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Icon(Icons.warning),
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: Text(
+                      errorMessage,
+                      style: Theme.of(context).textTheme.headline,
+                    ),
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.close),
+                  onPressed: _closeDialog,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screen_widget.dart
@@ -0,0 +1,149 @@
+library screens;
+
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:screens/error_dialog_widget.dart';
+
+class ScreenWidget extends StatefulWidget {
+  // ignore: close_sinks
+  final StreamController<String> errorStreamController = StreamController();
+  // ignore: close_sinks
+  final StreamController<bool> loaderStreamController = StreamController();
+
+  final Widget child;
+  final List<Widget> children;
+  final bool showAppBar;
+  final String appBarText;
+  final Widget appBarWidget;
+  final bool isAccent;
+  final bool isStatic;
+  final EdgeInsetsGeometry padding;
+  final BottomNavigationBar bottomNavigationBar;
+
+  ScreenWidget({
+    Key key,
+    this.showAppBar = false,
+    this.isAccent = false,
+    this.isStatic = false,
+    this.appBarText = '',
+    this.appBarWidget,
+    this.padding,
+    this.child,
+    this.children,
+    this.bottomNavigationBar,
+  })  : assert(child != null || children != null),
+        super(key: key);
+
+  @override
+  _ScreenWidgetState createState() => _ScreenWidgetState();
+}
+
+class _ScreenWidgetState extends State<ScreenWidget> {
+  double _height;
+
+  @override
+  dispose() {
+    widget.errorStreamController.close();
+    widget.loaderStreamController.close();
+    super.dispose();
+  }
+
+  _setHeight(double height) {
+    if (_height == null) {
+      _height = height;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    final _appBar = AppBar(
+      backgroundColor:
+          widget.isAccent ? theme.accentColor : theme.scaffoldBackgroundColor,
+      centerTitle: true,
+      title: widget.appBarWidget ??
+          Text(
+            widget.appBarText,
+          ),
+    );
+
+    return Stack(
+      children: <Widget>[
+        Scaffold(
+          backgroundColor: widget.isAccent ? theme.accentColor : null,
+          appBar: widget.showAppBar ? _appBar : null,
+          body: Container(
+            child: SafeArea(
+              child: (widget.child != null)
+                  ? LayoutBuilder(
+                      builder:
+                          (BuildContext context, BoxConstraints constraints) {
+                        _setHeight(constraints.constrainHeight(
+                            MediaQuery.of(context).size.height));
+                        return SingleChildScrollView(
+                          child: widget.isStatic
+                              ? Container(
+                                  padding: widget.padding,
+                                  height: _height,
+                                  child: widget.child,
+                                )
+                              : widget.child,
+                        );
+                      },
+                    )
+                  : ListView(children: widget.children),
+            ),
+          ),
+          bottomNavigationBar: widget.bottomNavigationBar,
+        ),
+        _buildErrorDialog(context),
+        _buildLoader(context),
+      ],
+    );
+  }
+
+  Widget _buildErrorDialog(BuildContext context) => StreamBuilder<String>(
+        stream: widget.errorStreamController.stream,
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.data == null || snapshot.data.isEmpty) {
+            return Container();
+          }
+
+          return ErrorDialog(
+            errorMessage: snapshot.data,
+            previousStatusBarWhiteForeground: !widget.isAccent,
+            closeFunction: () {
+              widget.errorStreamController.add(null);
+            },
+          );
+        },
+      );
+
+  Widget _buildLoader(BuildContext context) => StreamBuilder<bool>(
+        stream: widget.loaderStreamController.stream,
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.data == null || !snapshot.data) {
+            return Container();
+          }
+
+          final theme = Theme.of(context);
+
+          return Container(
+            color: Colors.black.withOpacity(0.5),
+            height: double.infinity,
+            width: double.infinity,
+            child: Center(
+              child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(
+                  !widget.isAccent
+                      ? theme.accentColor
+                      : theme.scaffoldBackgroundColor,
+                ),
+              ),
+            ),
+          );
+        },
+      );
+}
